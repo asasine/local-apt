e4ad6a023851ad6055781e88197df725f11aaf47
Adam Sasine <asasine@users.noreply.github.com>
2026-02-24 10:17:11 -0800

    Replace anyhow for ConfiguredPackage

--- src/packages/mod.rs
+++ src/packages/mod.rs
@@ -1,85 +1,12 @@
 //! [`ConfiguredPackages`] contains all packages to be processed, parsed from the
 //! configuration file with [`from_config`][`ConfiguredPackages::from_config`].
 
-use crate::{external::get_deb_fields, paths::PoolDir};
-use anyhow::{Context, anyhow};
-use core::{error::Error, fmt::Display};
-use std::{
-    fs::{self, File},
-    io,
-    path::Path,
+mod configured_package;
+pub use configured_package::{
+    ConfiguredPackage, DownloadError, InvalidDebError, ProcessPackageError,
 };
-use tracing::{debug, info};
 
-/// A single package to be processed.
-///
-/// This object is parsed from the configuration file. It contains information to
-/// download and process the package.
-#[derive(Debug)]
-pub struct ConfiguredPackage {
-    /// The download URL for the ``.deb`` package. This should point directly to
-    /// a `.deb` file.
-    pub url: String,
-}
-
-impl ConfiguredPackage {
-    /// Download the package to `temp_dir`, verify it, and move it to the appropriate
-    /// location in `pool_dir`.
-    pub fn process<T: AsRef<Path>>(&self, pool_dir: &PoolDir, temp_dir: T) -> anyhow::Result<()> {
-        info!("Processing package from: {}", self.url);
-        let temp_file = temp_dir
-            .as_ref()
-            .join(format!("package-{}.deb", std::process::id()));
-
-        self.download_to(&temp_file)?;
-
-        // Extract package metadata to move to correct location in the pool
-        // This also validates that the deb file is well-formed
-        let [pkg_name, pkg_version, pkg_arch] =
-            get_deb_fields(&temp_file, &["Package", "Version", "Architecture"])
-                .context("Could not extract package fields")?;
-
-        let standard_debian_filename = format!("{}_{}__{}.deb", pkg_name, pkg_version, pkg_arch);
-        let target_dir = pool_dir
-            .package_dir(&pkg_name)
-            .ok_or_else(|| anyhow!("Package name is empty, cannot determine target directory"))?;
-
-        // Validation done, move the file
-        fs::create_dir_all(&target_dir).context("Failed to create target directory")?;
-        let target_path = target_dir.join(&standard_debian_filename);
-        fs::rename(&temp_file, &target_path)
-            .context("Failed to move package to target directory")?;
-
-        info!(
-            "Successfully installed {} to {}",
-            standard_debian_filename,
-            target_path.display()
-        );
-
-        Ok(())
-    }
-
-    /// Download the package to the specified path.
-    // TODO: timestamping to avoid redownloading if the same package already exists
-    // in the pool and shares a timestamp with the source (e.g., from HTTP headers)
-    fn download_to<P: AsRef<Path>>(&self, path: P) -> anyhow::Result<()> {
-        let mut response =
-            reqwest::blocking::get(&self.url).context("Failed to download package")?;
-
-        if !response.status().is_success() {
-            return Err(anyhow!(
-                "Download failed with status: {}",
-                response.status()
-            ));
-        }
-
-        let file = File::create(path).context("Failed to create temporary file")?;
-        let mut file = std::io::BufWriter::new(file);
-        let bytes_writte = io::copy(&mut response, &mut file)?;
-        debug!("Downloaded {} bytes from {}", bytes_writte, self.url);
-        Ok(())
-    }
-}
+use core::{error::Error, fmt::Display};
 
 /// All packages to be processed, parsed from the configuration file.
 pub struct ConfiguredPackages {
